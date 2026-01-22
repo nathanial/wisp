bbb433ba43055c82364e60996b7aa7ba7682cd69
Nathanial Hartman <nathanial.hartman@gmail.com>
2026-01-21 16:39:37 -0800

    tune wisp test timeouts

--- WispTests/Common.lean
+++ WispTests/Common.lean
@@ -9,5 +9,10 @@ import Staple
 open Crucible
 export Staple (String.containsSubstr)
 
+def defaultTimeoutMs : UInt64 := 10000
+def defaultConnectTimeoutMs : UInt64 := 5000
+
 -- Client is created at module level (pure operation)
 def client := Wisp.HTTP.Client.new
+  |>.withTimeout defaultTimeoutMs
+  |>.withConnectTimeout defaultConnectTimeoutMs
